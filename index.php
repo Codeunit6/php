04ab6627dd6de3a43328998e6c67ad8ddf966b6d
Codeunit6 <fernando.brayan.m.g@gmail.com>
2021-10-25 18:02:50 -0500

    Actualizacion 1.0.0

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     $Direccion="Reyes Acozac la palma";
+     $Sexo="Masculino";
+     $Nombre="Fernando Brayan Mejia Gomez";
+     echo "<h1>PHP</h1>";
+     echo "<br>Nombre: <input type text value='$Nombre' style='position: relative; width: 300px;'>";
+     echo "<br>Sexo:   <input type text value='$Sexo' style='position: relative; width: 300px;'>";
+     echo "<br>Direccion: <input type text value='$Direccion'style='position: relative; width: 300px;'>";
+    ?>
+</body>
+</html>
